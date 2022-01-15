d7c464579f56ad74d1333acf7703a8e1a7de9c61
susannejung <95688233+susannejung@users.noreply.github.com>
2022-01-15 10:31:18 +0100

    4

--- src/FilEksempler/Main.java
+++ src/FilEksempler/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.print("2. udgave");
         System.out.print("3. udgave");
         System.out.print("4. udgave");
+        System.out.print("5. udgave");
 
     }
 }
